0934cb2041f3446120cacf1a2ade5c7f69a87ee7
jm-kim <jm-kim@linecorp.com>
2018-08-09 01:33:49 +0900

    init commit

---
+++ src/main/java/com/jongmin/App.java
@@ -0,0 +1,7 @@
+package com.jongmin;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("This is test project");
+    }
+}
---
+++ src/test/java/com/jongmin/AppTest.java
@@ -0,0 +1,13 @@
+package com.jongmin;
+
+import static junit.framework.TestCase.assertTrue;
+
+import org.junit.Test;
+
+public class AppTest {
+
+    @Test
+    public void simpleTest() {
+        assertTrue(true);
+    }
+}
